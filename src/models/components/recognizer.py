4c92227103d3d2b609488cae72a33ba1f18c53e3
nhngan <hnagn2003@gmail.com>
2023-04-16 21:58:49 +0700

    fix recognizer

--- src/models/components/recognizer.py
+++ src/models/components/recognizer.py
@@ -159,6 +159,35 @@ class RecognizerZelda(BaseModel):
         else:
             raise RuntimeError(f'Invalid mode: {mode}')
 
+class SimpleRecog(nn.Module):
+    def __init__(
+        self,
+        type='RecognizerZelda',
+        type_backbone='BackBoneZelda',
+        type_cls_head='ClsHeadZelda',
+        num_classes=2,
+        in_channels=128,
+        average_clips='prob'
+    ):
+        super().__init__()
+        backbone=dict(type=type_backbone),
+        cls_head=dict(
+            type=type_cls_head,
+            num_classes=num_classes,
+            in_channels=in_channels,
+            average_clips=average_clips
+        )
+        model_cfg = dict(
+            type=type,
+            backbone=dict(type='BackBoneZelda'),
+            cls_head=dict(
+                type='ClsHeadZelda',
+                num_classes=2,
+                in_channels=128,
+                average_clips='prob'))
+        self.model = MODELS.build(model_cfg)
+    def forward(self, x):
+        return self.model.forward(x)
 if __name__ == "__main__":
 #     model = SimpleRecog("mmaction2/configs/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x5-100e_kinetics400-rgb.py",
 #                         'src/models/checkpoints/tsn_r50_video_1x1x8_100e_kinetics400_rgb_20200702-568cde33.pth',
@@ -171,21 +200,12 @@ if __name__ == "__main__":
 #     print(output)
     # _ = SimpleRecog()
     
-    model_cfg = dict(
-        type='RecognizerZelda',
-        backbone=dict(type='BackBoneZelda'),
-        cls_head=dict(
-            type='ClsHeadZelda',
-            num_classes=2,
-            in_channels=128,
-            average_clips='prob'))
-
-    model = MODELS.build(model_cfg)
+    _ = SimpleRecog()
     x = torch.randn(2,10,3,10,224,224) #batch frame channel segments H W
     # device = torch.device('cuda')
     # x = x.to( torch.device(device))
 # print(x.shape)
-    output = model.forward(x)
+    output = _.forward(x)
     print(output)
     print(output.shape)
     
---
+++ src/models/recog_eval_module.py
@@ -0,0 +1,65 @@
+import copy
+from collections import OrderedDict
+from mmengine.evaluator import BaseMetric
+from mmaction.evaluation import top_k_accuracy
+from mmaction.registry import METRICS
+import torch
+
+@METRICS.register_module()
+class AccuracyMetric(BaseMetric):
+    def __init__(self, topk=(1, 5), collect_device='cpu', prefix='acc'):
+        super().__init__(collect_device=collect_device, prefix=prefix)
+        self.topk = topk
+
+    def process(self, data_batch, data_samples):
+        data_samples = copy.deepcopy(data_samples)
+        for data_sample in data_samples:
+            result = dict()
+            scores = data_sample['pred_scores']['item'].cpu().numpy()
+            label = data_sample['gt_labels']['item'].item()
+            result['scores'] = scores
+            result['label'] = label
+            self.results.append(result)
+
+    def compute_metrics(self, results: list) -> dict:
+        eval_results = OrderedDict()
+        labels = [res['label'] for res in results]
+        scores = [res['scores'] for res in results]
+        topk_acc = top_k_accuracy(scores, labels, self.topk)
+        for k, acc in zip(self.topk, topk_acc):
+            eval_results[f'topk{k}'] = acc
+        return eval_results
+
+
+if __name__ == "__main__":
+    # read config file from configs/model/dlib_resnet.yaml
+    import pyrootutils
+    from omegaconf import DictConfig
+    import hydra
+
+    # find paths
+    path = pyrootutils.find_root(
+        search_from=__file__, indicator=".project-root")
+    config_path = str(path / "configs" / "model")
+    output_path = path / "outputs"
+    print("paths", path, config_path, output_path)
+
+    # def test_net(cfg):
+    #     net = hydra.utils.instantiate(cfg.net)
+    #     print("*"*20+" net "+"*"*20, "\n", net)
+    #     output = net(torch.randn(1, 10, 3, 224, 224))
+    #     print("output", output.shape)
+
+    def test_module(cfg):
+        metric_cfg = dict(type=cfg.metrics_type, topk=cfg.topk)
+        # module = hydra.utils.instantiate(cfg)
+        # output = module(torch.randn(1, 10, 3, 224, 224))
+        # print("module output", output.shape)
+
+    @hydra.main(version_base="1.3", config_path=config_path, config_name="recog.yaml")
+    def main(cfg: DictConfig):
+        print(cfg)
+        # test_net(cfg)
+        test_module(cfg)
+
+    main()
